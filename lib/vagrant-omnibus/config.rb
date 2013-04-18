1b189c97f47c05f7272c7792b59101d253efc197
Seth Chisamore <schisamo@opscode.com>
2013-04-17 20:45:23 -0400

    resolve `latest` to a real Chef version; fixes GH-2
    Queries the RubyGems.org's Ruby API to determine the latest version of
    Chef.
    
    The `InstallChef` action does not install the Omnibus Chef package if
    the requested version is already installed. Resolving a version of
    `latest` to a real version (i.e. 11.4.0) allows this check to succeed
    when appropriate.

--- lib/vagrant-omnibus/config.rb
+++ lib/vagrant-omnibus/config.rb
@@ -1,4 +1,6 @@
-require "vagrant"
+require 'rubygems/dependency'
+require 'rubygems/dependency_installer'
+require 'vagrant'
 
 module VagrantPlugins
   module Omnibus
@@ -14,7 +16,45 @@ module VagrantPlugins
       end
 
       def finalize!
-        @chef_version = nil if @chef_version == UNSET_VALUE
+        if @chef_version == UNSET_VALUE
+          @chef_version = nil
+        elsif @chef_version.to_s == 'latest'
+          # resolve `latest` to a real version
+          @chef_version = retrieve_latest_chef_version
+        end
+      end
+
+      private
+
+      # Query RubyGems.org's Ruby API and retrive the latest version of Chef.
+      def retrieve_latest_chef_version
+        latest_version = nil
+
+        available_gems = dependency_installer.find_gems_with_sources(chef_gem_dependency)
+        spec, source = if available_gems.respond_to?(:last)
+                          # DependencyInstaller sorts the results such that the last one is
+                          # always the one it considers best.
+                          spec_with_source = available_gems.last
+                          spec_with_source
+                        else
+                          # Rubygems 2.0 returns a Gem::Available set, which is a
+                          # collection of AvailableSet::Tuple structs
+                          available_gems.pick_best!
+                          best_gem = available_gems.set.first
+                          best_gem && [best_gem.spec, best_gem.source]
+                        end
+
+        latest_version = spec && spec.version
+
+        latest_version
+      end
+
+      def dependency_installer
+        @dependency_installer ||= Gem::DependencyInstaller.new
+      end
+
+      def chef_gem_dependency(version=nil)
+        Gem::Dependency.new('chef', version)
       end
     end
   end
