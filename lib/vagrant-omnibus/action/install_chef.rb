ef15bf591230319e2041eed29a8877a833ddc256
Shawn Dahlen <shawn@dahlen.me>
2013-06-19 15:45:39 -0400

    Remove provider dependencies.
    This commit is an initial cut at removing provider dependencies. It also
    streamlines the action middleware.
    
    Testing is not complete.

--- lib/vagrant-omnibus/action/install_chef.rb
+++ lib/vagrant-omnibus/action/install_chef.rb
@@ -26,39 +26,52 @@ module VagrantPlugins
 
         def initialize(app, env)
           @app = app
-          # Config#finalize! SHOULD be called automatically
-          env[:global_config].omnibus.finalize!
+          @machine = env[:machine]
         end
 
         def call(env)
-          desired_chef_version = env[:global_config].omnibus.chef_version
+          @app.call(env)
 
-          unless desired_chef_version.nil?
-            env[:ui].info("Ensuring Chef is installed at requested version of #{desired_chef_version}.")
-            if env[:installed_chef_version] == desired_chef_version
-              env[:ui].info("Chef #{desired_chef_version} Omnibus package is already installed...skipping installation.")
+          return unless @machine.communicate.ready?
+
+          desired_version = @machine.config.omnibus.chef_version
+          unless desired_version.nil?
+            if installed_version == desired_version
+              env[:ui].info I18n.t('vagrant-omnibus.action.installed', {
+                :version => desired_version
+              })
             else
-              env[:ui].info("Chef #{desired_chef_version} Omnibus package is not installed...installing now.")
-              env[:ssh_run_command] = install_chef_command(desired_chef_version)
+              env[:ui].info I18n.t('vagrant-omnibus.action.installing', {
+                :version => desired_version
+              })
+              install(desired_version)
             end
           end
-
-          @app.call(env)
         end
 
         private
 
-        def install_chef_command(version=latest)
-          <<-INSTALL_OMNIBUS
-if command -v wget &>/dev/null; then
-  wget -qO- #{INSTALL_SH} | sudo bash -s -- -v #{version}
-elif command -v curl &>/dev/null; then
-  curl -L #{INSTALL_SH} -v #{version} | sudo bash
-else
-  echo "Neither wget nor curl found. Please install one and try again." >&2
-  exit 1
-fi
-INSTALL_OMNIBUS
+        def installed_version
+          version = nil
+          command = 'echo $(chef-solo --v | awk "{print \\$2}" || "")'
+          @machine.communicate.sudo(command) do |type, data|
+            version = data.chomp if [:stderr, :stdout].include?(type)
+          end
+          version
+        end
+
+        def install(version)
+          command = <<-INSTALL_OMNIBUS
+            if command -v wget &>/dev/null; then
+              wget -qO- #{INSTALL_SH} | sudo bash -s -- -v #{version}
+            elif command -v curl &>/dev/null; then
+              curl -L #{INSTALL_SH} -v #{version} | sudo bash
+            else
+              echo "Neither wget nor curl found. Please install one." >&2
+              exit 1
+            fi
+          INSTALL_OMNIBUS
+          @machine.communicate.sudo(command)
         end
       end
     end
