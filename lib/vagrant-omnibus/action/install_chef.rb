13eb9b48195a13a5120956f46e9e789922626533
jagorjat <j.gorjatsev@gmail.com>
2014-01-05 17:28:20 +0200

    support of windows guests

--- lib/vagrant-omnibus/action/install_chef.rb
+++ lib/vagrant-omnibus/action/install_chef.rb
@@ -26,14 +26,44 @@ module VagrantPlugins
       #
       # This action installs Chef Omnibus packages at the desired version.
       class InstallChef
-        INSTALL_SH = "#{ENV['OMNIBUS_INSTALL_URL'] ||
-          'https://www.opscode.com/chef/install.sh'}"
-
+        
         def initialize(app, env)
           @app = app
+          @is_windows_guest = windows_guest?(env)
           @logger =
             Log4r::Logger.new('vagrantplugins::omnibus::action::installchef')
           @machine = env[:machine]
+          @install_sh = ENV['OMNIBUS_INSTALL_URL'] || "https://www.opscode.com/chef/install.sh"
+          @local_install_name = "install.sh"
+          if @is_windows_guest && !ENV['OMNIBUS_INSTALL_URL']
+              # create URL to get desired chef version
+              # adapted from https://github.com/opscode/knife-windows/blob/master/lib/chef/knife/bootstrap/windows-chef-client-msi.erb
+              version = @machine.config.omnibus.chef_version
+              windows_version = /Version (?<major_minor>\d+\.\d+)\.(?<build>\d+)/.match(`cmd /c ver`)
+              case windows_version[:major_minor]
+              when "5.2"
+                  machine_os = "2003r2"
+              when "6.0"
+                  machine_os = "2008"
+              when "6.1"
+                  machine_os = "2008r2"
+              when "6.2", "6.3"
+                  machine_os = "2012"
+              else
+                  machine_os = "2008r2"
+              end
+              # detect architecture
+              windows_arch = ENV["PROCESSOR_ARCHITEW6432"] || ENV["PROCESSOR_ARCHITECTURE"]
+              case windows_arch.downcase
+              when "amd64"
+                  machine_arch = "x86_64"
+              else
+                  machine_arch = "i686"
+              end
+              @install_sh = "https://www.opscode.com/chef/download?p=windows&pv=#{machine_os}&m=#{machine_arch}&v=#{version}"
+              @local_install_name = "install.bat"
+              @logger.info("Chef windows installer will be taken from: #{@install_sh}")
+          end
           # Config#finalize! SHOULD be called automatically
           @machine.config.omnibus.finalize!
         end
@@ -42,8 +72,7 @@ module VagrantPlugins
           @app.call(env)
 
           return if !@machine.communicate.ready? ||
-                    !provision_enabled?(env) ||
-                    windows_guest?(env)
+                    !provision_enabled?(env)
 
           desired_version = @machine.config.omnibus.chef_version
           unless desired_version.nil?
@@ -76,8 +105,14 @@ module VagrantPlugins
 
         def installed_version
           version = nil
-          command = 'echo $(chef-solo -v)'
-          @machine.communicate.sudo(command) do |type, data|
+          opts = nil
+          if @is_windows_guest
+            command = 'cmd.exe /c chef-solo -v 2>&0'
+            opts = {:shell => :cmd, :error_check => false}
+          else
+            command = 'echo $(chef-solo -v)'
+          end
+          @machine.communicate.sudo(command, opts) do |type, data|
             if [:stderr, :stdout].include?(type)
               next if data =~ /stdin: is not a tty/
               v = data.chomp
@@ -87,15 +122,19 @@ module VagrantPlugins
           version
         end
 
-        # Uploads install.sh from Host's Vagrant TMP directory to guest
+        # Uploads install.sh or install.bat from Host's Vagrant TMP directory to guest
         # and executes.
         def install(version, env)
           shell_escaped_version = Shellwords.escape(version)
 
           @machine.communicate.tap do |comm|
-            comm.upload(@install_sh_temp_path, 'install.sh')
-            # TODO: Execute with `sh` once install.sh removes it's bash-isms.
-            install_cmd = "bash install.sh -v #{shell_escaped_version} 2>&1"
+            comm.upload(@install_sh_temp_path, @local_install_name)
+            if @is_windows_guest
+              install_cmd = "cmd.exe /c #{@local_install_name}"
+            else
+              # TODO: Execute with `sh` once install.sh removes it's bash-isms.
+              install_cmd = "bash #{@local_install_name} -v #{shell_escaped_version} 2>&1"
+            end
             comm.sudo(install_cmd) do |type, data|
               if [:stderr, :stdout].include?(type)
                 next if data =~ /stdin: is not a tty/
@@ -105,7 +144,7 @@ module VagrantPlugins
           end
         end
 
-        # Fetches install.sh file to the Host's Vagrant TMP directory.
+        # Fetches install.sh or creates install.bat file to the Host's Vagrant TMP directory.
         #
         # Mostly lifted from:
         #
@@ -113,10 +152,10 @@ module VagrantPlugins
         #
         def fetch_install_sh(env)
           @install_sh_temp_path =
-            env[:tmp_path].join(Time.now.to_i.to_s + '-install.sh')
-          @logger.info("Downloading install.sh to: #{@install_sh_temp_path}")
+            env[:tmp_path].join(Time.now.to_i.to_s + "-#{@local_install_name}")
+          @logger.info("Downloading #{@local_install_name} to: #{@install_sh_temp_path}")
 
-          url = INSTALL_SH
+          url = @install_sh
           if File.file?(url) || url !~ /^[a-z0-9]+:.*$/i
             @logger.info('Assuming URL is a file.')
             file_path = File.expand_path(url)
@@ -128,14 +167,28 @@ module VagrantPlugins
           # downloader_options[:insecure] = env[:box_download_insecure]
           # downloader_options[:ui] = env[:ui]
 
-          # Download the install.sh file to a temporary path. We store
+          # Download the install.sh or create install.bat file to a temporary path. We store
           # the temporary path as an instance variable so that the
           # `#recover` method can access it.
           begin
-            downloader = Vagrant::Util::Downloader.new(url,
-                                                       @install_sh_temp_path,
-                                                       downloader_options)
-            downloader.download!
+            if @is_windows_guest
+                # create install.bat file in @install_sh_temp_path location
+                File.open(@install_sh_temp_path, 'w') do |f|
+                    f.puts <<-eos
+                    @echo off
+                    set dest=%~dp0\install.msi
+                    echo "Downloading Chef from #{url} to %dest%"
+                    powershell -command "(New-Object System.Net.WebClient).DownloadFile('#{url}', '%dest%')"
+                    echo Running installation %dest%
+                    msiexec /q /i %dest%
+                    eos
+                end
+            else
+                downloader = Vagrant::Util::Downloader.new(url,
+                                                           @install_sh_temp_path,
+                                                           downloader_options)
+                downloader.download!
+            end
           rescue Vagrant::Errors::DownloaderInterrupted
             # The downloader was interrupted, so just return, because that
             # means we were interrupted as well.
